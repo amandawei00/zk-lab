f5c831f65f1f5b77055839953cf8e27892a97280
amandawei00 <amandawei00@gmail.com>
2022-01-19 11:39:14 -0800

    abraham lincoln was a wrestler and only lost 1 out of roughly 300 matches

--- bk/bk_interpolate_interp2d.py
+++ bk/bk_interpolate_interp2d.py
@@ -11,7 +11,7 @@ class N:
 
     def __init__(self):
         self.n_ = 400
-        self.x0 = 0.02
+        self.x0 = 0.01
         self.xr1 = np.log(3.e-6)
         # self.xr1 = 0.0
         self.xr2 = np.log(60.e0)  # limit of integration in fourier transform calculation
@@ -22,27 +22,20 @@ class N:
 
 
         # read results.csv file from BK solution to pandas dataframe
-        self.df = pd.read_csv("full_bk_results.csv", sep="\t")
-        self.df.columns = ['kuta', 'y', 'vr', 'vfr', 'prev']
-        # self.df = self.df.drop(self.df[self.df.kuta == "kuta"].index)
+        self.df = pd.read_csv("results.csv", sep="\t") 
+        self.df.columns = ['y', 'vr', 'vfr']
 
         # converting dataframe element types
-        self.df["kuta"] = self.df["kuta"].astype('int')
         self.df["y"] = (self.df["y"].astype('float32')).round(decimals=1)
         self.df["vr"] = self.df["vr"].astype('float64')
         self.df["vfr"] = self.df["vfr"].astype('float64')
 
-        self.df = self.df.loc[(self.df['kuta'] == 4)]  # only consider 4th-order RK solutions
 
-        self.r = np.concatenate(np.array(self.df.loc[self.df['y'] == 0.][['vr']]))  # r values over which N(r,Y) are evaluated
+        self.r = np.concatenate(np.array(self.df.loc[self.df['y'] == 0.][['vr']]))  # r values for N interp
         self.y = np.unique(np.array(self.df[['y']]))
 
         self.z = [self.df.loc[(self.df['y'] == yy) & ((self.df['vr'] < (rr + tol)) & (self.df['vr'] > (rr - tol)))][['vfr']].iloc[0]['vfr'] for rr in self.r for yy in self.y]
         self.f = interp.interp2d(self.r, self.y, self.z, kind='cubic')
-
-        print(self.f)
-        print(type(self.f))
-        print("grid created, interpolated")
 
     # finds location of val in grid in the sense that if val is between two elements in grid, find_index will return the index of the lower element
     def find_index(self, val, grid):
@@ -71,29 +64,20 @@ class N:
         return 2 * self.master(r, y) - np.power(self.master(r, y), 2)
 
     def udg_f(self, x, k):
-        t1 = time.time()
         y_ = np.log(self.x0 / x)
-        integrand = lambda r_: (1 - self.master(r_, y_)) * self.bessel(k * r_, 0)
+        integrand = lambda r_: (1 - self.master(r_, y_)) * self.bessel(k * r_, 0)  # * r_?
         a = 2 * np.pi * intg.quad(integrand, self.r[0], self.r[len(self.r)-1], epsabs=1.e-4)[0]
-        t2 = time.time()
-        print("udg_f took " + str(t2-t1) + " seconds")
         return a
 
     def udg_a(self, x, k):
-        t1 = time.time()
         y_ = np.log(self.x0 / x)
         integrand = lambda r_: (1 - self.master_adj(r_, y_)) * self.bessel(k * r_, 0)
         a = 2 * np.pi * intg.quad(integrand, self.r[0], self.r[len(self.r)-1], epsabs=1.e-4)[0]
-        t2 = time.time()
-        print("udg_a took " + str(t2-t1) + " seconds")
         return a
 
     def bessel(self, x, alpha):
-        # print("entering bessel")
         f = lambda t: np.cos(alpha * t - x * np.sin(t))
-        g = (1 / np.pi) * intg.quad(f, 0, np.pi, epsabs=1.e-3)[0]
-        # print("exiting bessel")
-        return g
+        return (1 / np.pi) * intg.quad(f, 0, np.pi, epsabs=1.e-3)[0]
 
 # end of class
 
