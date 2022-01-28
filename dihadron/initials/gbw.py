f7e286f49e0f0c2f041747391b7de0da99bb9540
amandawei00 <amandawei00@gmail.com>
2022-01-28 09:28:49 -0800

    awofjwef

--- dihadron/initials/gbw.py
+++ dihadron/initials/gbw.py
@@ -1,4 +1,5 @@
 from numpy import exp
+from sympy import symbols, diff, lambdify
 
 # qsq0 [GeV^2] = 1 [GeV^2]: initial saturation scale
 # x0           = 0.01     : onset of small-x
@@ -6,3 +7,5 @@ from numpy import exp
 def s(x, r_, x0=0.01, qsq0=1.0, gamma=0.3):
     qsq = qsq0 * np.power(x0/x, gamma)
     return exp(-(1/4) * qsq * r_ * r_)
+
+
--- fits/fit_solver.py
+++ fits/fit_solver.py
@@ -29,21 +29,24 @@ for i in error_name:
     data['err(tot)'] += data[i] * data[i]
 data['err(tot)'] = data['err(tot)'].apply(np.sqrt)
 
-# print(data)
-'''
 qsq2 = np.array(data.qsq2)
 x = np.array(data.x)
 
+'''
 parameters:
    1. qsq20: initial saturation scale
    2. c    :
    3. gamma: anomalous dimension thing
    4. sigma: normalization factor
+'''
 
+# constructing theory
+def fth(q, c, g, s):
+    
 def chi_squared(qsq20, c, gamma, sigma):
-    # run BK for given parameters qsq2, c, and gamma
+    # run BK for given parameters qsq2, c, sigma, and gamma
     res = 0
-    for i in range(len(f2_dat)):
+    for i in range(len(data)):
         theory = f2(x[i], qsq2[i])
         exp = f2_dat[i]
         err = f2_err[i]
@@ -51,5 +54,5 @@ def chi_squared(qsq20, c, gamma, sigma):
     return res
 
 chi_squared.errordef = Minuit.LEAST_SQUARES
-m = Minuit(chi_squared, qsq20=10.08, c=1, gamma=1)
-m.simplex()'''
+m = Minuit(chi_squared, qsq20=0.4, c=1, gamma=1)
+m.simplex()
