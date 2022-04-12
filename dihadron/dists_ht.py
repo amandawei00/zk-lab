95d9544f25a792dff83b82c70c8508d41c6c4f44
amandawei00 <amandawei00@gmail.com>
2022-04-12 01:33:07 -0700

    added dists_ht.py in dihadron folder for calculations of TMDs for rcBK without having to calculate laplacian!! I think. I will check with Farid tomorrw

--- dihadron/dists_ht.py
+++ dihadron/dists_ht.py
@@ -1,17 +1,19 @@
 import numpy as np
 from scipy.integrate import quad
 import matplotlib.pyplot as plt
-from scipy import special
+from scipy.special import j0
 from scipy.fft import fht
+from scipy.signal import convolve
 from integrator import osc_intg
 
 import sys
 sys.path.append('./initials/')
 
-# init
+
+# init 
 initial = 'mv'
 x0      = 0.01
-g       = 0.3 
+g       = 0.3
 
 if initial is 'mv':
     import mv as pig
@@ -23,80 +25,77 @@ elif initial is 'gbw':
 def qsq(x):
     return qsq0 * np.power(x0/x, g)
 
-q2 = qsq(x0)
+q2      = qsq(x0)
+# function parameters
+n       = 399
+r1      = 3.e-6
+r2      = 60.e0
+xr1     = np.log(r1)
+xr2     = np.log(r2)
+hr      = (xr2 - xr1) / n
+
+xlr_    = [xr1 + i * hr for i in range(n + 1)]
+r_      = np.exp(xlr_)
+
+klr_    = [1/xlr_[i] for i in range(len(xlr_))]
+k_      = [1/r_[i] for i in range(len(r_))]
+print(k_)
 
 # parameters
 A  = 197
 nc = 3
 cf = (nc * nc - 1)/(2 * nc)
 s_ = 1
-
+c0 = cf * s_/(4 * np.pi * np.pi * np.pi)
+c1 = nc * s_/(4 * np.pi * np.pi * np.pi)
 # args = [x, k]
 
-def qg1(u, *args):
-    x   = args[0]
-    k   = args[1]
-    pre = nc * s_/(4 * np.pi * np.pi * np.pi)
-    jac = u / (k * k)
-    bes = special.j0(u)
-    return pre * jac * bes * pig.lap(x, u/k) 
+def h(f, g, c0):
+    # hankel transform functions f and g
+    f_ = fht(f, dln=hr, mu=0) # dln=np.exp(hr)?
+    g_ = fht(g, dln=hr, mu=0)
 
-def qg2(u, *args):
-    x   = args[0]
-    k   = args[1]
-    pre = cf * s_/(4 * np.pi * np.pi * np.pi)
+    # convolve f and g
+    c  = convolve(f_, g_)
+    return -(1/(2 * np.pi)) * interp1d(k_, c)
 
-    jac = u / (k * k)
-    bes = special.j0(u)
-    mid = pig.kap(x, u/k) * (1 - np.power(pig.s(x, u/k), nc/cf))
-    return pre * jac * bes * mid * pig.s(x, u/k)
+def qg1_f(x, r):
+    return 1 - pig.s(x, r)
 
-def gg1(u, *args):
-    x   = args[0]
-    k   = args[1]
-    pre = nc * s_/(4 * np.pi * np.pi * np.pi)
-    jac = u / (k * k)
+def qg1_g(x, r):
+    return c1
 
-    bes = special.j0(u)
-    return pre * jac * bes * pig.s(x, u/k) * pig.lap(x, u/k)
+def qg2_f(x, r):
+    return - np.log(pig.s(x, r))
 
-def adj(u, *args):
-    x   = args[0]
-    k   = args[1]
-    pre = cf * s_/(4 * np.pi * np.pi * np.pi)
-    jac = u / (k * k)
+def qg2_g(x, r):
+    m = pig.s(x, r)
+    return ((1 - np.power(m, nc/cf)) * m)/(-np.log(m))
 
-    bes = special.j0(u)
-    mid = pig.lap2(x, u/k, nc/cf)
-    return pre * jac * bes * mid
+def gg1_f(x, r):
+    return 1 - pig.s(x, r)
 
-def ww(u, *args):
-    x   = args[0]
-    k   = args[1]
-    pre = cf * s_/(4 * np.pi * np.pi * np.pi)
-    jac = u / (k * k)
+def gg1_g(x, r):
+    return c1 * pig.s(x, r)
 
-    bes = special.j0(u)
-    mid = pig.kap(x, u/k) * (1 - np.power(pig.s(x, u/k), nc/cf))
-    return pre * jac * bes * mid
+def adj_f(x, r):
+    return 1 - np.power(pig.s(x, r), nc/cf)
 
-def gg6(u, *args):
-    x   = args[0]
-    k   = args[1]
-    pre = cf * s_/(4 * np.pi * np.pi * np.pi)
-    jac = u / (k * k)
+def adj_g(x, r):
+    return c1
 
-    bes = special.j0(u)
-    mid = pig.kap(x, u/k) * (1 - np.power(pig.s(x, u/k), nc/cf))
-    end = np.power(pig.s(x, u/k), nc/cf)
+def ww_f(x, r):
+    return -np.log(pig.s(x, r))
 
-    return pre * jac * bes * mid * end    
+def ww_g(x, r):
+    m = pig.s(x, r)
+    return c0 * (1 - np.power(m, nc/cf))/(-m)
 
 def dist(f, x, k_):
     res = osc_intg(f, 1000, x, k_)
     print('F(k  = ' + str(k_) + ') = ' + str(res))
     return res
-
+'''
 k = np.logspace(-2, 2, 100)
 qg1_ = [dist(qg1, 0.01, i) for i in k]
 qg2_ = [dist(qg2, 0.01, i) for i in k]
@@ -122,3 +121,4 @@ plt.plot(k, gg6_, 'green', linestyle='--', linewidth=lw, label='gg(6)')
 plt.plot(k, ww_, 'cyan', linestyle='-', linewidth=lw, label='WW')
 plt.legend()
 plt.show()
+'''
--- fits/run_fit.x
+++ fits/run_fit.x
@@ -1,1 +1,1 @@
-qsub -cwd -V -N fit -l h_data=4G,h_rt=08:00:00,highp -pe shared 5 $PWD/run.bash
+qsub -cwd -V -N fit -l h_data=4G,h_rt=100:00:00,highp -pe shared 5 $PWD/run.bash
