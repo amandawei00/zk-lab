659b148a57837aa0c2c6fd6c9e93390dde583b1a
amandawei00 <amandawei00@gmail.com>
2022-06-02 14:30:20 -0700

    oawkefjw

--- bk/bk_solver.py
+++ bk/bk_solver.py
@@ -71,11 +71,11 @@ def evolve():
     with Pool(processes=5) as pool:
         k1 = np.array(pool.map(intg, xlr_, chunksize=80))
 
-    k1 = list(k1 * hy)
+    k1 = list(k1 * hy/2)
     so.set_k(xlr_, k1)
     with Pool(processes=5) as pool:
         k2 = np.array(pool.map(intg, xlr_, chunksize=80))
-    return 0.5 * hy * (k1 + k2)
+    return k2 * hy
 
     # return (1/6) * hy * (k1 + 2 * k2 + 2 * k3 + k4)
 
